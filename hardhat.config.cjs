11324bb227e049960e852d949fee578f4e28e6b0
onedionys <onedionys@gmail.com>
2025-01-19 11:35:19 +0700

    ```

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,25 @@
-require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+const process = require('process');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: '0.8.28',
+    solidity: {
+        version: '0.8.28', // Pastikan ini sesuai dengan pragma Solidity
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    networks: {
+        'tea-assam': {
+            url: process.env.RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: Number(process.env.CHAIN_ID),
+        },
+    },
 };
