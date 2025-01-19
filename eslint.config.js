366da7a4a4ac0211a52066b36665e80ba9564ae7
onedionys <onedionys@gmail.com>
2025-01-19 11:35:18 +0700

    fix: add eslint-plugin-mocha to improve test suite linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import mochaPlugin from 'eslint-plugin-mocha';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    mochaPlugin.configs.flat.recommended,
+];
