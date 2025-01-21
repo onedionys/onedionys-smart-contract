100eda0a2cbbcd65019fadd14b72763841a555b9
onedionys <onedionys@gmail.com>
2025-01-22 01:52:55 +0700

    test: refactor quiz contract tests to use a generic token contract instead of onedionystoken

--- test/Quiz.test.js
+++ test/Quiz.test.js
@@ -7,25 +7,25 @@ use(chaiAsPromised);
 
 describe('Quiz Contract', function () {
     let owner, user1, user2;
-    let quiz, odtToken;
+    let quiz, token;
 
     beforeEach(async function () {
         [owner, user1, user2] = await ethers.getSigners();
 
-        const ODTToken = await ethers.getContractFactory('OneDionysToken');
-        odtToken = await ODTToken.deploy();
-        await odtToken.deployed();
+        const Token = await ethers.getContractFactory('Token');
+        token = await Token.deploy();
+        await token.deployed();
 
-        await odtToken.mint(user1.address, ethers.utils.parseEther('100'));
-        await odtToken.mint(user2.address, ethers.utils.parseEther('100'));
+        await token.mint(user1.address, ethers.utils.parseEther('100'));
+        await token.mint(user2.address, ethers.utils.parseEther('100'));
 
         const Quiz = await ethers.getContractFactory('Quiz');
-        quiz = await Quiz.deploy(odtToken.address);
+        quiz = await Quiz.deploy(token.address);
         await quiz.deployed();
 
         const rewardAmount = ethers.utils.parseEther('500');
-        await odtToken.mint(owner.address, rewardAmount);
-        await odtToken.approve(quiz.address, rewardAmount);
+        await token.mint(owner.address, rewardAmount);
+        await token.approve(quiz.address, rewardAmount);
         await quiz.addRewardTokens(rewardAmount);
     });
 
@@ -35,16 +35,16 @@ describe('Quiz Contract', function () {
             const ownerShare = ethers.utils.parseEther('5');
             const contractShare = ethers.utils.parseEther('10');
 
-            const userInitialBalance = await odtToken.balanceOf(user1.address);
-            const ownerInitialBalance = await odtToken.balanceOf(owner.address);
-            const contractInitialBalance = await odtToken.balanceOf(quiz.address);
+            const userInitialBalance = await token.balanceOf(user1.address);
+            const ownerInitialBalance = await token.balanceOf(owner.address);
+            const contractInitialBalance = await token.balanceOf(quiz.address);
 
-            await odtToken.connect(user1).approve(quiz.address, joinAmount);
+            await token.connect(user1).approve(quiz.address, joinAmount);
             await quiz.connect(user1).joinQuiz();
 
-            const userFinalBalance = await odtToken.balanceOf(user1.address);
-            const ownerFinalBalance = await odtToken.balanceOf(owner.address);
-            const contractFinalBalance = await odtToken.balanceOf(quiz.address);
+            const userFinalBalance = await token.balanceOf(user1.address);
+            const ownerFinalBalance = await token.balanceOf(owner.address);
+            const contractFinalBalance = await token.balanceOf(quiz.address);
 
             expect(userFinalBalance.toString()).to.equal(userInitialBalance.sub(joinAmount).toString());
             expect(contractFinalBalance.toString()).to.equal(contractInitialBalance.add(contractShare).toString());
@@ -54,6 +54,11 @@ describe('Quiz Contract', function () {
 
     describe('Submit Answer', function () {
         it('Should increase user points if the answer is correct', async function () {
+            const joinAmount = ethers.utils.parseEther('15');
+
+            await token.connect(user1).approve(quiz.address, joinAmount);
+            await quiz.connect(user1).joinQuiz();
+
             await quiz.connect(user1).submitAnswer(true);
 
             const points = await quiz.userPoints(user1.address);
@@ -61,6 +66,11 @@ describe('Quiz Contract', function () {
         });
 
         it('Should not increase points if the answer is incorrect', async function () {
+            const joinAmount = ethers.utils.parseEther('15');
+
+            await token.connect(user1).approve(quiz.address, joinAmount);
+            await quiz.connect(user1).joinQuiz();
+            
             await quiz.connect(user1).submitAnswer(false);
 
             const points = await quiz.userPoints(user1.address);
@@ -72,7 +82,7 @@ describe('Quiz Contract', function () {
         it('Should allow users to claim rewards for their points', async function () {
             const joinAmount = ethers.utils.parseEther('15');
 
-            await odtToken.connect(user1).approve(quiz.address, joinAmount);
+            await token.connect(user1).approve(quiz.address, joinAmount);
             await quiz.connect(user1).joinQuiz();
 
             await quiz.connect(user1).submitAnswer(true);
@@ -83,7 +93,7 @@ describe('Quiz Contract', function () {
 
             await quiz.connect(user1).claimRewards();
 
-            const userBalance = await odtToken.balanceOf(user1.address);
+            const userBalance = await token.balanceOf(user1.address);
             expect(userBalance.toString()).to.equal(ethers.utils.parseEther('91').toString());
 
             const contractRewardPool = await quiz.totalRewardPool();
@@ -98,8 +108,8 @@ describe('Quiz Contract', function () {
     describe('Add Reward Tokens', function () {
         it('Should allow owner to add reward tokens', async function () {
             const additionalReward = ethers.utils.parseEther('500');
-            await odtToken.mint(owner.address, additionalReward);
-            await odtToken.approve(quiz.address, additionalReward);
+            await token.mint(owner.address, additionalReward);
+            await token.approve(quiz.address, additionalReward);
 
             await quiz.addRewardTokens(additionalReward);
 
@@ -109,8 +119,8 @@ describe('Quiz Contract', function () {
 
         it('Should reject non-owner trying to add reward tokens', async function () {
             const additionalReward = ethers.utils.parseEther('500');
-            await odtToken.mint(user1.address, additionalReward);
-            await odtToken.connect(user1).approve(quiz.address, additionalReward);
+            await token.mint(user1.address, additionalReward);
+            await token.connect(user1).approve(quiz.address, additionalReward);
 
             await expect(quiz.connect(user1).addRewardTokens(additionalReward)).to.be.rejectedWith(
                 'OwnableUnauthorizedAccount',
