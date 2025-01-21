4ade22ad1e1dd97c3d3b26d61510e82dde06bace
onedionys <onedionys@gmail.com>
2025-01-22 01:19:13 +0700

    feat: enhance staking contract with event emissions and reward calculations

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -13,12 +13,22 @@ contract Staking is Ownable {
     mapping(address => uint256) public stakedAmounts;
     mapping(address => uint256) public stakingTimestamp;
 
+    event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
+    event RewardsClaimed(address indexed user, uint256 reward);
+    event RewardTokensAdded(uint256 amount);
+    event RewardPerSecondUpdated(uint256 newRewardPerSecond);
+
     constructor(address _odtToken) Ownable(msg.sender) {
         odtToken = IERC20(_odtToken);
     }
 
     function stake(uint256 amount) external {
         require(amount > 0, 'Amount should be greater than 0');
+        require(odtToken.balanceOf(msg.sender) >= amount, 'Insufficient balance for staking');
+
+        uint256 allowance = odtToken.allowance(msg.sender, address(this));
+        require(allowance >= amount, 'Allowance not sufficient for staking');
 
         odtToken.transferFrom(msg.sender, address(this), amount);
 
@@ -28,6 +38,8 @@ contract Staking is Ownable {
 
         stakedAmounts[msg.sender] += amount;
         stakingTimestamp[msg.sender] = block.timestamp;
+
+        emit Staked(msg.sender, amount);
     }
 
     function unstake(uint256 amount) external {
@@ -41,6 +53,8 @@ contract Staking is Ownable {
         if (stakedAmounts[msg.sender] == 0) {
             stakingTimestamp[msg.sender] = 0;
         }
+
+        emit Unstaked(msg.sender, amount);
     }
 
     function claimRewards() external {
@@ -58,16 +72,33 @@ contract Staking is Ownable {
         totalRewardPool -= reward;
         odtToken.transfer(user, reward);
         stakingTimestamp[user] = block.timestamp;
+
+        emit RewardsClaimed(user, reward);
     }
 
     function addRewardTokens(uint256 amount) external onlyOwner {
         require(amount > 0, 'Amount should be greater than 0');
+
+        uint256 allowance = odtToken.allowance(msg.sender, address(this));
+        require(allowance >= amount, 'Allowance not sufficient to add reward tokens');
+
         odtToken.transferFrom(msg.sender, address(this), amount);
         totalRewardPool += amount;
+
+        emit RewardTokensAdded(amount);
     }
 
     function setRewardPerSecond(uint256 newRewardPerSecond) external onlyOwner {
         require(newRewardPerSecond > 0, 'Reward per second must be greater than 0');
         rewardPerSecond = newRewardPerSecond;
+
+        emit RewardPerSecondUpdated(newRewardPerSecond);
+    }
+
+    function calculatePendingRewards(address user) public view returns (uint256) {
+        uint256 stakedAmount = stakedAmounts[user];
+        uint256 stakingDuration = block.timestamp - stakingTimestamp[user];
+        uint256 reward = (rewardPerSecond * stakedAmount * stakingDuration) / 1e18;
+        return reward;
     }
 }
