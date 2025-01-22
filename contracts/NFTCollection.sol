218bfe5c3e1dad48669bdf0e044964afab7194a2
onedionys <onedionys@gmail.com>
2025-01-22 09:09:33 +0700

    feat: enhance nft minting functionality by adding rarity and points, integrating with a lottery contract

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -7,34 +7,56 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 contract NFTCollection is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
 
-    constructor() ERC721('DionysNFT', 'DNFT') Ownable(msg.sender) {}
+    mapping(uint256 => NFTDetails) public nftDetails;
+    address public lotteryContract;
 
-    // Function to mint a new NFT, only callable by the contract owner
-    function mint(address to) external onlyOwner {
-        _safeMint(to, nextTokenId);
-        _setTokenURI(
-            nextTokenId,
-            string(abi.encodePacked('https://ipfs.io/ipfs/your_hash_', uint2str(nextTokenId), '.json'))
-        );
-        nextTokenId++;
+    struct NFTDetails {
+        string rarity;
+        uint256 points;
     }
 
-    // Helper function to convert a uint256 value to a string (used to generate dynamic token IDs)
+    event NFTMinted(address indexed to, uint256 tokenId, string rarity, uint256 points);
+
+    constructor() ERC721('DionysNFT', 'DNFT') Ownable(msg.sender) {}
+
+    modifier onlyLotteryContract() {
+        require(msg.sender == lotteryContract, 'Caller is not the lottery contract');
+        _;
+    }
+
+    function setLotteryContract(address _lotteryContract) external onlyOwner {
+        lotteryContract = _lotteryContract;
+    }
+
+    function mint(address to, string memory rarity, uint256 points) external onlyLotteryContract {
+        uint256 tokenId = nextTokenId;
+        _safeMint(to, tokenId);
+        nftDetails[tokenId] = NFTDetails(rarity, points);
+
+        _setTokenURI(tokenId, string(abi.encodePacked('https://ipfs.io/ipfs/your_hash_', uint2str(tokenId), '.json')));
+
+        nextTokenId++;
+
+        emit NFTMinted(to, tokenId, rarity, points);
+    }
+
+    function getNFTDetails(uint256 tokenId) external view returns (string memory, uint256) {
+        NFTDetails memory details = nftDetails[tokenId];
+        return (details.rarity, details.points);
+    }
+
     function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
-        // If the value is 0, return "0" as a string
         if (_i == 0) {
             return '0';
         }
         uint256 j = _i;
         uint256 length;
-        // Count the number of digits in the number
         while (j != 0) {
             length++;
             j /= 10;
         }
         bytes memory bstr = new bytes(length);
         uint256 k = length - 1;
-        // Convert each digit of the number to a character in the string
         while (_i != 0) {
             bstr[k--] = bytes1(uint8(48 + (_i % 10)));
             _i /= 10;
