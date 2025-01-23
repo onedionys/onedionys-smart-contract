00138c2de1a73aa4775b40b1eaeba37f2f4a4124
onedionys <onedionys@gmail.com>
2025-01-23 09:05:20 +0700

    feat: add nft burning functionality, user token tracking, and rarity metadata management to the nftcollection contract

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -6,8 +6,9 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract NFTCollection is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
-
     mapping(uint256 => NFTDetails) public nftDetails;
+    mapping(address => uint256[]) private userTokens;
+    mapping(string => string) public rarityMetadataCID;
     address public lotteryContract;
 
     struct NFTDetails {
@@ -17,8 +18,18 @@ contract NFTCollection is ERC721URIStorage, Ownable {
     }
 
     event NFTMinted(address indexed to, uint256 tokenId, string rarity, uint256 points, string cid);
+    event NFTBurned(address indexed owner, uint256 tokenId);
 
-    constructor() ERC721('DionysNFT', 'DNFT') Ownable(msg.sender) {}
+    constructor() ERC721('DionysNFT', 'DNFT') Ownable(msg.sender) {
+        rarityMetadataCID['Common'] = 'bafkreiglegydzpdxrvszs554pogs7uuvc5iqykmsibk6dtcbiohqrgnohu';
+        rarityMetadataCID['Uncommon'] = 'bafkreihxegzellny3ftx3zcti2aorl5n7iwdkb7cdwwdlvna5hq3e2hgom';
+        rarityMetadataCID['Rare'] = 'bafkreiejp4jbxq3jc6utjvehlflga37rhqd7eudr5fasucvo5ffaqnmfje';
+        rarityMetadataCID['Epic'] = 'bafkreicqyeyz4bnn4jb4f3r3y6fl7524grxrcddflvy5rccylo6saa5l6y';
+        rarityMetadataCID['Legendary'] = 'bafkreienytu72bahf5jbf4u5477niaeull5s2pl2dk4pz6cses5cbajnya';
+        rarityMetadataCID['Mythic'] = 'bafkreidw7qtpq2n2qte66hqftpavcfdyamo44nwqsn2sfachjvgtofyqpu';
+        rarityMetadataCID['Godlike'] = 'bafkreicmouxjpsgabx7rwtnwavj5lllstwdiihbovht2ajspadpbfp2bmm';
+        rarityMetadataCID['Unique'] = 'bafkreiaijtv5ipk3asyvnecjkqpemfpbuzu2janzihbdabwn4z4qnhktm4';
+    }
 
     modifier onlyLotteryContract() {
         require(msg.sender == lotteryContract, 'Caller is not the lottery contract');
@@ -34,18 +45,10 @@ contract NFTCollection is ERC721URIStorage, Ownable {
 
         _safeMint(to, tokenId);
 
-        string memory metadataCID;
-        if (keccak256(abi.encodePacked(rarity)) == keccak256(abi.encodePacked('Common'))) {
-            metadataCID = 'bafkreifph4d7ekx6iapuc6jjilbzzxwtv4e722x5wegrgynem2drqjmgxq';
-        } else if (keccak256(abi.encodePacked(rarity)) == keccak256(abi.encodePacked('Rare'))) {
-            metadataCID = 'bafkreih2cmazhjjb64ezclpszqkmq7penj5pukle45qq6mbgiszt7isoua';
-        } else if (keccak256(abi.encodePacked(rarity)) == keccak256(abi.encodePacked('Epic'))) {
-            metadataCID = 'bafkreib6p7sv23uigc3trcqvkffhrh73x7b3xzs5i35ofnaysrz53pdpzm';
-        } else if (keccak256(abi.encodePacked(rarity)) == keccak256(abi.encodePacked('Legendary'))) {
-            metadataCID = 'bafkreib5lvtpafojuzozawae3o4bqvbhs76utylitjvtnfgpshxh6sx2oq';
-        }
+        string memory metadataCID = rarityMetadataCID[rarity];
 
         nftDetails[tokenId] = NFTDetails(rarity, points, metadataCID);
+        userTokens[to].push(tokenId);
         _setTokenURI(tokenId, string(abi.encodePacked('https://ipfs.io/ipfs/', metadataCID)));
 
         nextTokenId++;
@@ -56,13 +59,36 @@ contract NFTCollection is ERC721URIStorage, Ownable {
     }
 
     function burn(uint256 tokenId) external onlyLotteryContract {
+        address owner = ownerOf(tokenId);
+        _removeTokenFromUser(owner, tokenId);
+
         _burn(tokenId);
         delete nftDetails[tokenId];
+        emit NFTBurned(owner, tokenId);
+    }
+
+    function getUserTokens(address user) external view returns (uint256[] memory) {
+        return userTokens[user];
+    }
+
+    function _removeTokenFromUser(address user, uint256 tokenId) private {
+        uint256[] storage tokens = userTokens[user];
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i] == tokenId) {
+                tokens[i] = tokens[tokens.length - 1];
+                tokens.pop();
+                break;
+            }
+        }
     }
 
     function getNFTDetails(uint256 tokenId) external view returns (string memory, uint256, string memory) {
         NFTDetails memory details = nftDetails[tokenId];
         return (details.rarity, details.points, details.cid);
+    }
+
+    function setRarityMetadataCID(string memory rarity, string memory cid) external onlyOwner {
+        rarityMetadataCID[rarity] = cid;
     }
 
     function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
