e624b5deebb224b0814d29b67106874fe92bc660
onedionys <onedionys@gmail.com>
2025-01-27 07:37:36 +0700

    refactor: renamed the token name and symbol in the token contract from 'stofmap token v3' and 'sft3' to 'one dionys token' and 'odt' respectively

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, Ownable {
     event TokensMinted(address indexed to, uint256 amount);
     event TokensBurned(address indexed burner, uint256 amount);
 
-    constructor() ERC20('Stofmap Token V3', 'SFT3') Ownable(msg.sender) {
+    constructor() ERC20('One Dionys Token', 'ODT') Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
     }
 
