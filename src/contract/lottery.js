4f8fc82c3167f35e4119a95159ae29bd9c9b7f36
onedionys <onedionys@gmail.com>
2025-01-28 11:29:03 +0700

    feat: add contract interaction files for all smart contracts, enabling interaction with the blockchain

---
+++ src/contract/lottery.js
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers';
+import process from 'process';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const getABI = (toPath = '') => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.resolve(__dirname, `./artifacts/contracts/${toPath}`);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+
+    return JSON.parse(fileContent);
+};
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const userWallet = new ethers.Wallet(process.env.USER_PRIVATE_KEY, provider);
+
+const contractAddress = process.env.LOTTERY_CONTRACT_ADDRESS;
+const contractJson = getABI('Lottery.sol/Lottery.json');
+const contractAbi = contractJson.abi;
+const contractInteraction = new ethers.Contract(contractAddress, contractAbi, mainWallet);
+
+const leaderboardAddress = process.env.LEADERBOARD_CONTRACT_ADDRESS;
+const leaderboardJson = getABI('Leaderboard.sol/Leaderboard.json');
+const leaderboardAbi = leaderboardJson.abi;
+const leaderboardInteraction = new ethers.Contract(leaderboardAddress, leaderboardAbi, mainWallet);
+
+console.log(userWallet, contractInteraction, leaderboardInteraction);
