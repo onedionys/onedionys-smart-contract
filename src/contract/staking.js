d216185848e50ceeaa610bb34bae4163b1be3e71
onedionys <onedionys@gmail.com>
2025-02-01 18:48:08 +0700

    feat: implement staking and unstaking functionalities with reward claiming and leaderboard integration

--- src/contract/staking.js
+++ src/contract/staking.js
@@ -1,31 +1,90 @@
 import { ethers } from 'ethers';
 import process from 'process';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
-// import ora from 'ora';
-
-const getABI = (toPath = '') => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const filePath = path.resolve(__dirname, `./artifacts/contracts/${toPath}`);
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-
-    return JSON.parse(fileContent);
-};
+import ora from 'ora';
+import { getErrorMessage, getJsonABI } from './../utils.js';
+import { addActivity } from './leaderboard.js';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const userWallet = new ethers.Wallet(process.env.USER_PRIVATE_KEY, provider);
 
 const contractAddress = process.env.STAKING_CONTRACT_ADDRESS;
-const contractJson = getABI('Staking.sol/Staking.json');
+const contractJson = getJsonABI('Staking.sol/Staking.json');
 const contractAbi = contractJson.abi;
 const contractInteraction = new ethers.Contract(contractAddress, contractAbi, mainWallet);
 
-const leaderboardAddress = process.env.LEADERBOARD_CONTRACT_ADDRESS;
-const leaderboardJson = getABI('Leaderboard.sol/Leaderboard.json');
-const leaderboardAbi = leaderboardJson.abi;
-const leaderboardInteraction = new ethers.Contract(leaderboardAddress, leaderboardAbi, mainWallet);
+async function addRewardTokensStaking(amount) {
+    const rewardAmount = ethers.utils.parseUnits(amount.toString(), 18);
 
-console.log(userWallet, contractInteraction, leaderboardInteraction);
+    const approveTx = await tokenContract.approve(stakingContractAddress, rewardAmount);
+    await approveTx.wait();
+    console.log(`Approved ${amount} token for reward pool.`);
+
+    const addRewardTx = await stakingContract.addRewardTokens(rewardAmount);
+    await addRewardTx.wait();
+    console.log(`Added ${amount} token as reward tokens.`);
+}
+
+async function stakeToken(wallet, amount) {
+    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
+    const stakeAmount = ethers.utils.parseUnits(amount.toString(), 18);
+
+    const approveTx = await tokenContract.connect(walletInstance).approve(stakingContractAddress, stakeAmount);
+    await approveTx.wait();
+    console.log(`Approved ${amount} token for staking contract by ${wallet.address}`);
+
+    const stakingTx = await stakingContract.connect(walletInstance).stake(stakeAmount);
+    const receipt = await stakingTx.wait();
+    const txHash = receipt.transactionHash;
+
+    const addActivityTx = await leaderboardContract.addActivity(
+        wallet.address,
+        'Stake Token',
+        'Users stake token',
+        amount,
+        txHash,
+    );
+    await addActivityTx.wait();
+    console.log(`Staked ${amount} token by ${wallet.address}`);
+}
+
+async function unstakeToken(wallet, amount) {
+    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
+    const stakeAmount = ethers.utils.parseUnits(amount.toString(), 18);
+
+    const approveTx = await tokenContract.connect(walletInstance).approve(stakingContractAddress, stakeAmount);
+    await approveTx.wait();
+    console.log(`Approved ${amount} token for unstaking contract by ${wallet.address}`);
+
+    const unstakingTx = await stakingContract.connect(walletInstance).unstake(stakeAmount);
+    const receipt = await unstakingTx.wait();
+    const txHash = receipt.transactionHash;
+
+    const addActivityTx = await leaderboardContract.addActivity(
+        wallet.address,
+        'Unstake Token',
+        'Users unstake token',
+        amount,
+        txHash,
+    );
+    await addActivityTx.wait();
+
+    console.log(`Unstaked ${amount} token by ${wallet.address}`);
+}
+
+async function claimRewards(wallet) {
+    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
+
+    const tx = await stakingContract.connect(walletInstance).claimRewards();
+    const receipt = await tx.wait();
+    const txHash = receipt.transactionHash;
+
+    const addActivityTx = await leaderboardContract.addActivity(
+        wallet.address,
+        'Claim Rewards Token',
+        'Users claim rewards token',
+        1000,
+        txHash,
+    );
+    await addActivityTx.wait();
+    console.log(`Rewards claimed by ${wallet.address}`);
+}
