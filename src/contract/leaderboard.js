ebe178a8265e3d03b86c182a9b316824a1f10354
onedionys <onedionys@gmail.com>
2025-02-01 13:28:17 +0700

    feat: implement addactivity function to the leaderboard contract, enabling activity logging

--- src/contract/leaderboard.js
+++ src/contract/leaderboard.js
@@ -1,26 +1,51 @@
 import { ethers } from 'ethers';
 import process from 'process';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
-// import ora from 'ora';
-
-const getABI = (toPath = '') => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const filePath = path.resolve(__dirname, `./artifacts/contracts/${toPath}`);
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-
-    return JSON.parse(fileContent);
-};
+import ora from 'ora';
+import { getErrorMessage, getJsonABI } from './../utils.js';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const userWallet = new ethers.Wallet(process.env.USER_PRIVATE_KEY, provider);
 
 const contractAddress = process.env.LEADERBOARD_CONTRACT_ADDRESS;
-const contractJson = getABI('Leaderboard.sol/Leaderboard.json');
+const contractJson = getJsonABI('Leaderboard.sol/Leaderboard.json');
 const contractAbi = contractJson.abi;
 const contractInteraction = new ethers.Contract(contractAddress, contractAbi, mainWallet);
 
-console.log(userWallet, contractInteraction);
+export async function addActivity(address = '', title = '', description = '', amount = 0, transactionHash = '') {
+    const spinner = ora('Loading...').start();
+
+    try {
+        const transaction = await contractInteraction.addActivity(address, title, description, amount, transactionHash);
+        await transaction.wait();
+        spinner.stop();
+
+        console.log(`📝 Successfully added activity to leaderboard list`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred when adding an activity: ${getErrorMessage(error)}`);
+    }
+}
+
+export async function getUsers() {}
+
+export async function getActivities() {}
+
+// async function getAllUsersLeaderboard() {
+//     const users = await leaderboardContract.getAllUsers();
+//     const walletData = await Promise.all(
+//         users.map(async (wallet) => {
+//             const activities = await leaderboardContract.getActivities(wallet);
+//             return {
+//                 wallet,
+//                 activityCount: activities.length,
+//             };
+//         }),
+//     );
+
+//     console.log(walletData);
+// }
+
+// async function getUsersActivity(wallet) {
+//     const tx = await leaderboardContract.getActivities(wallet.address);
+//     console.log(tx);
+// }
