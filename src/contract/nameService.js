5ea94b66b0f6b4925821d539dbab75e2ed5dc6c3
onedionys <onedionys@gmail.com>
2025-02-01 18:47:55 +0700

    feat: implement name registration, retrieval, and ownership checking functions for the nameservice contract, adding withdraw functionality

--- src/contract/nameService.js
+++ src/contract/nameService.js
@@ -1,31 +1,47 @@
 import { ethers } from 'ethers';
 import process from 'process';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
-// import ora from 'ora';
-
-const getABI = (toPath = '') => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const filePath = path.resolve(__dirname, `./artifacts/contracts/${toPath}`);
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-
-    return JSON.parse(fileContent);
-};
+import ora from 'ora';
+import { getErrorMessage, getJsonABI } from './../utils.js';
+import { addActivity } from './leaderboard.js';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const userWallet = new ethers.Wallet(process.env.USER_PRIVATE_KEY, provider);
 
 const contractAddress = process.env.NAME_SERVICE_CONTRACT_ADDRESS;
-const contractJson = getABI('NameService.sol/NameService.json');
+const contractJson = getJsonABI('NameService.sol/NameService.json');
 const contractAbi = contractJson.abi;
 const contractInteraction = new ethers.Contract(contractAddress, contractAbi, mainWallet);
 
-const leaderboardAddress = process.env.LEADERBOARD_CONTRACT_ADDRESS;
-const leaderboardJson = getABI('Leaderboard.sol/Leaderboard.json');
-const leaderboardAbi = leaderboardJson.abi;
-const leaderboardInteraction = new ethers.Contract(leaderboardAddress, leaderboardAbi, mainWallet);
+async function nameServiceRegister(wallet, name) {
+    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
 
-console.log(userWallet, contractInteraction, leaderboardInteraction);
+    const tx = await nameServiceContract.connect(walletInstance).registerName(name, {
+        value: ethers.utils.parseUnits('20', 18),
+    });
+    await tx.wait();
+    console.log('User registered successfully!');
+}
+
+async function nameServiceGetName(address) {
+    const name = await nameServiceContract.getName(address);
+    if (name === '') {
+        console.log(`No name registered for address: ${address}`);
+    } else {
+        console.log(`Name registered for address ${address}: ${name}`);
+    }
+}
+
+async function nameServiceGetOwner(name) {
+    const owner = await nameServiceContract.getOwner(name);
+    if (owner === '') {
+        console.log(`No owner registered for name: ${name}`);
+    } else {
+        console.log(`Owner registered for name ${name}: ${owner}`);
+    }
+}
+
+async function nameServiceWithdraw() {
+    const tx = await nameServiceContract.withdrawFunds();
+    console.log('Transaction sent:', tx.hash);
+    await tx.wait();
+}
