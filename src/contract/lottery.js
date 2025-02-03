03eba92aa02632f754b08394c3c03a562871079e
onedionys <onedionys@gmail.com>
2025-02-03 12:36:19 +0700

    feat: implement leaderboard retrieval, nft burning, and token withdrawal functionalities in lottery contract

--- src/contract/lottery.js
+++ src/contract/lottery.js
@@ -4,10 +4,10 @@ import ora from 'ora';
 import { getErrorMessage, getJsonABI } from './../utils.js';
 import { addActivity } from './leaderboard.js';
 import { tokenContract } from './token.js';
+import { nftCollectionContract /* , setLotteryContract */ } from './nftCollection.js';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const userWallet = new ethers.Wallet(process.env.USER_PRIVATE_KEY, provider);
 
 const contractAddress = process.env.LOTTERY_CONTRACT_ADDRESS;
 const contractJson = getJsonABI('Lottery.sol/Lottery.json');
@@ -96,7 +96,9 @@ export async function spinWheel(wallet) {
         spinner.stop();
 
         console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
-        console.log(`✅ Successfully spin the wheel and won 1 ${rarityNft} NFT, costing ${amountSpinFormatted} tokens.`);
+        console.log(
+            `✅ Successfully spin the wheel and won 1 ${rarityNft} NFT (worth ${amountPointsFormatted}), costing ${amountSpinFormatted} tokens.`,
+        );
     } catch (error) {
         spinner.stop();
         console.log(error);
@@ -104,91 +106,130 @@ export async function spinWheel(wallet) {
     }
 }
 
+export async function getLeaderboard() {
+    const spinner = ora('Loading...').start();
+
+    try {
+        const [addresses, points] = await contractInteraction.getLeaderboard();
+        const leaderboard = addresses.map((addr, index) => ({
+            address: addr,
+            points: points[index].toNumber(),
+        }));
+
+        spinner.stop();
+        return leaderboard;
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while getting the leaderboard list: ${getErrorMessage(error)}`);
+        return [];
+    }
+}
+
+export async function burnNft(wallet, tokenId = 0) {
+    console.log(`🤖 Processing: Burning NFTs`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
+
+    try {
+        const connectWallet = contractInteraction.connect(wallet);
+
+        const [rarity, points] = await nftCollectionContract.getNFTDetails(tokenId);
+        const transaction = await connectWallet.burnNft(tokenId);
+        const receipt = await transaction.wait();
+
+        let amountPoints = points.toNumber();
+
+        await addActivity(
+            wallet.address,
+            'NFT Burn',
+            `Burned 1 ${rarity} NFT and get ${amountPoints} tokens.`,
+            amountPoints,
+            receipt.transactionHash,
+        );
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully burned 1 ${rarity} NFT with ID ${tokenId}`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while burning the NFT: ${getErrorMessage(error)}`);
+    }
+}
+
+export async function withdrawTokens(wallet) {
+    console.log(`🤖 Processing: Spin Rewards Withdrawal`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
+
+    try {
+        const iface = new ethers.utils.Interface([
+            'event Withdraw(address indexed user, uint256 points)',
+        ]);
+
+        const connectWallet = contractInteraction.connect(wallet);
+
+        const transaction = await connectWallet.withdrawTokens();
+        const receipt = await transaction.wait();
+
+        let amountPoints = 0;
+
+        receipt.logs.some((log) => {
+            try {
+                const parsedLog = iface.parseLog(log);
+                if (parsedLog.name === 'Withdraw') {
+                    amountPoints = parseFloat(parsedLog.args.points);
+                    return true;
+                }
+            } catch (e) {
+                void e;
+            }
+            return false;
+        });
+
+        const amountPointsFormatted = amountPoints.toLocaleString('en-US');
+
+        await addActivity(
+            wallet.address,
+            'Spin Rewards Withdrawal',
+            `Claimed ${amountPointsFormatted} tokens from the prize spin wheel.`,
+            amountPoints,
+            receipt.transactionHash,
+        );
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully withdrawn all tokens (${amountPointsFormatted}) won from the lottery`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while withdrawing all tokens: ${getErrorMessage(error)}`);
+    }
+}
+
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
+// await setLotteryContract();
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
 // await addRewardsLottery(1000);
-await spinWheel(userWallet);
-
-// async function spinWheel(wallet) {
-//     const iface = new ethers.utils.Interface([
-//         'event SpinWheel(address indexed user, string rarity, uint256 points, string cid)',
-//     ]);
-//     const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
-//     const amount = ethers.utils.parseUnits('10', 18);
-
-//     const approveTx = await tokenContract.connect(walletInstance).approve(lotteryContractAddress, amount);
-//     await approveTx.wait();
-
-//     const tx = await lotteryContract.connect(walletInstance).spinWheel();
-//     const receipt = await tx.wait();
-//     const txHash = receipt.transactionHash;
-
-//     const addActivityTx = await leaderboardContract.addActivity(
-//         wallet.address,
-//         'Spin Wheel',
-//         'Users spin wheel',
-//         500,
-//         txHash,
-//     );
-//     await addActivityTx.wait();
-
-//     for (const log of receipt.logs) {
-//         try {
-//             const parsedLog = iface.parseLog(log);
-//             if (typeof parsedLog.args[0] != 'undefined') {
-//                 console.log(
-//                     `Spin completed, ${parsedLog.args.user} gets an NFT ${parsedLog.args.rarity} with ${parsedLog.args.points.toNumber()} points!`,
-//                 );
-//             }
-//         } catch (e) {
-//             void e;
-//         }
-//     }
-// }
-
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
 // await spinWheel(mainWallet);
-// await addRewardsLottery(1000);
-
-// async function getLeaderboard() {
-//     const [addresses, points] = await lotteryContract.getLeaderboard();
-//     console.log('Leaderboard:');
-//     addresses.forEach((addr, index) => {
-//         console.log(`${index + 1}. ${addr} - ${points[index]} points`);
-//     });
-// }
-
-// async function withdrawTokens(wallet) {
-//     const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
-
-//     const tx = await lotteryContract.connect(walletInstance).withdrawTokens();
-//     const receipt = await tx.wait();
-//     const txHash = receipt.transactionHash;
-
-//     const addActivityTx = await leaderboardContract.addActivity(
-//         wallet.address,
-//         'Withdraw Token',
-//         'Users withdraw token',
-//         1000,
-//         txHash,
-//     );
-//     await addActivityTx.wait();
-//     console.log('Tokens successfully withdrawn from the lottery contract!');
-// }
-
-
-
-// async function burnNft(wallet, tokenId) {
-//     const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
-
-//     const tx = await lotteryContract.connect(walletInstance).burnNft(tokenId);
-//     const receipt = await tx.wait();
-//     const txHash = receipt.transactionHash;
-
-//     const addActivityTx = await leaderboardContract.addActivity(
-//         wallet.address,
-//         'Burn NFT',
-//         'Users burn nft',
-//         1000,
-//         txHash,
-//     );
-//     await addActivityTx.wait();
-//     console.log(`NFT with Token ID ${tokenId} has been burned.`);
-// }
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
+// const leaderboard = await getLeaderboard();
+// console.log(leaderboard);
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
+// await burnNft(mainWallet, 1);
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
+// await withdrawTokens(mainWallet);
+// console.log(" ");
+// console.log("======================================");
+// console.log(" ");
