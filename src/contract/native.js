9086072956c9e185f8b7e960bbb8bfe48d56eb90
onedionys <onedionys@gmail.com>
2025-02-03 22:09:43 +0700

    feat: enhance native contract interaction with improved error handling and user feedback, adding deposit, withdrawal, balance check, faucet claim, and donation functionality

--- src/contract/native.js
+++ src/contract/native.js
@@ -17,58 +17,170 @@ const nativeContractAddress = contractAddress;
 
 export { nativeContract, nativeContractAddress };
 
-async function depositNativeToken(amount) {
-    const poolAmount = ethers.utils.parseEther(amount.toString());
+export async function deposit(amount) {
+    console.log(`🤖 Processing: Deposit Native Token`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
 
-    const addRewardTx = await nativeContract.depositNative({
-        value: poolAmount,
-    });
-    await addRewardTx.wait();
-    console.log(`Deposit ${amount} token as reward tokens.`);
+    try {
+        amount = ethers.utils.parseUnits(amount.toString(), 18);
+        const amountPool = parseFloat(ethers.utils.formatUnits(amount, 18));
+        const amountPoolFormatted = amountPool.toLocaleString('en-US');
+
+        const transaction = await contractInteraction.depositNative({
+            value: amount
+        });
+        const receipt = await transaction.wait();
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully deposited ${amountPoolFormatted} tokens as a reward.`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while depositing tokens: ${getErrorMessage(error)}`);
+    }
 }
 
-async function withdrawNativeToken(amount) {
-    const poolAmount = ethers.utils.parseEther(amount.toString());
+export async function getBalance() {
+    const spinner = ora('Loading...').start();
 
-    const addRewardTx = await nativeContract.withdrawNative(poolAmount);
-    await addRewardTx.wait();
-    console.log(`Withdraw ${amount} token as reward tokens.`);
+    try {
+        const amount = await contractInteraction.getContractBalance();
+        const amountPool = parseFloat(ethers.utils.formatEther(amount));
+        spinner.stop();
+
+        return amountPool || 0;
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while getting the contract balance: ${getErrorMessage(error)}`);
+        return 0;
+    }
 }
 
-async function getContractBalanceNativeToken() {
-    const addRewardTx = await nativeContract.getContractBalance();
-    console.log(`Contract balance. ${ethers.utils.formatEther(addRewardTx)}`);
+export async function claimFaucet(wallet) {
+    console.log(`🤖 Processing: Faucet Claim Native`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
+
+    try {
+        const iface = new ethers.utils.Interface(['event FaucetClaimed(address indexed claimer, uint256 amount)']);
+
+        const connectWallet = contractInteraction.connect(wallet);
+
+        const transaction = await connectWallet.claimFaucet();
+        const receipt = await transaction.wait();
+
+        let amountClaimed = 0;
+
+        receipt.logs.some((log) => {
+            try {
+                const parsedLog = iface.parseLog(log);
+                if (parsedLog.name === 'FaucetClaimed') {
+                    amountClaimed = parseFloat(ethers.utils.formatUnits(parsedLog.args.amount, 18));
+                    return true;
+                }
+            } catch (e) {
+                void e;
+            }
+            return false;
+        });
+
+        const amountClaimedFormatted = amountClaimed.toLocaleString('en-US');
+
+        await addActivity(
+            wallet.address,
+            'Faucet Claim Native',
+            `Claimed ${amountClaimedFormatted} tokens from the faucet native.`,
+            amountClaimed,
+            receipt.transactionHash,
+        );
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully claim faucet for address ${wallet.address}`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while claiming the faucet native: ${getErrorMessage(error)}`);
+    }
 }
 
-async function claimFaucetNativeToken(wallet) {
-    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
+export async function donate(wallet, amount) {
+    console.log(`🤖 Processing: Donate Native Token`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
 
-    const tx = await nativeContract.connect(walletInstance).claimFaucet();
-    const receipt = await tx.wait();
-    const txHash = receipt.transactionHash;
+    try {
+        amount = ethers.utils.parseUnits(amount.toString(), 18);
+        const amountDonated = parseFloat(ethers.utils.formatUnits(amount, 18));
+        const amountDonatedFormatted = amountDonated.toLocaleString('en-US');
 
-    const addActivityTx = await leaderboardContract.addActivity(
-        wallet.address,
-        'Claim Native Faucet',
-        'Users claim native faucet',
-        1000,
-        txHash,
-    );
-    await addActivityTx.wait();
-    console.log(`Succeess claim native token.`);
+        const connectWallet = contractInteraction.connect(wallet);
+
+        const transaction = await connectWallet.donate({
+            value: amount
+        });
+        const receipt = await transaction.wait();
+
+        await addActivity(
+            wallet.address,
+            'Donate Native Token',
+            `Donate ${amountDonatedFormatted} tokens to the native contract.`,
+            amountDonated,
+            receipt.transactionHash,
+        );
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully donated ${amountDonatedFormatted} tokens to a native contract.`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while making a token donation: ${getErrorMessage(error)}`);
+    }
 }
 
-async function donateNativeToken(wallet, amount) {
-    const walletInstance = new ethers.Wallet(wallet.privateKey, provider);
-    const stakeAmount = ethers.utils.parseEther(amount.toString());
+export async function withdraw(amount) {
+    console.log(`🤖 Processing: Native Withdrawal`);
+    console.log(`⏳ Current Time: ${new Date().toString()}`);
+    const spinner = ora('Loading...').start();
 
-    const unstakingTx = await nativeContract.connect(walletInstance).donate({
-        value: stakeAmount,
-    });
-    const receipt = await unstakingTx.wait();
-    const txHash = receipt.transactionHash;
+    try {
+        amount = ethers.utils.parseUnits(amount.toString(), 18);
+        const amountWithdrawal = parseFloat(ethers.utils.formatUnits(amount, 18));
+        const amountWithdrawalFormatted = amountWithdrawal.toLocaleString('en-US');
 
-    const addActivityTx = await leaderboardContract.addActivity(wallet.address, 'Donate', 'Users donate', 1000, txHash);
-    await addActivityTx.wait();
-    console.log(`Succeess donate native token.`);
+        const transaction = await contractInteraction.withdrawNative(amount);
+        const receipt = await transaction.wait();
+        spinner.stop();
+
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`✅ Successfully withdraw ${amountWithdrawalFormatted} tokens from the native contract`);
+    } catch (error) {
+        spinner.stop();
+        console.log(`❌ An error occurred while withdrawing tokens: ${getErrorMessage(error)}`);
+    }
 }
+
+console.log(" ");
+console.log("======================================");
+console.log(" ");
+await deposit(1);
+console.log(" ");
+console.log("======================================");
+console.log(" ");
+const balance = await getBalance();
+console.log(balance);
+console.log(" ");
+console.log("======================================");
+console.log(" ");
+await claimFaucet(mainWallet);
+console.log(" ");
+console.log("======================================");
+console.log(" ");
+await donate(mainWallet, 1);
+console.log(" ");
+console.log("======================================");
+console.log(" ");
+await withdraw(balance);
+console.log(" ");
+console.log("======================================");
+console.log(" ");
