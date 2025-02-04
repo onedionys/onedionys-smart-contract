eb263d94d1de26e1f287b20b2a0b0c8398119776
onedionys <onedionys@gmail.com>
2025-02-05 00:55:19 +0700

    fix: delete all angular-related files, specifically for smart contracts

--- ckeditor.config.js
+++
@@ -1,9 +0,0 @@
-import builder from '@ckeditor/ckeditor5-builder';
-import SourceEditing from '@ckeditor/ckeditor5-source-editing';
-import Toolbar from '@ckeditor/ckeditor5-ui/src/toolbar';
-
-const editorConfig = await builder.createEditorConfig({
-    plugins: [SourceEditing, Toolbar],
-});
-
-export default editorConfig;
