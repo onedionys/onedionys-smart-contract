2c63e4a74c3e33ec0bf493b501f1d287e58dbb11
onedionys <onedionys@gmail.com>
2025-03-06 15:28:46 +0700

    refactor: update rpc url and block explorer url environment variables to use reddio specific urls

--- src/contract/nftCollection.js
+++ src/contract/nftCollection.js
@@ -4,7 +4,7 @@ import ora from 'ora';
 import { getErrorMessage, getJsonABI } from './../utils.js';
 import { lotteryContractAddress } from './lottery.js';
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL_REDDIO);
 const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 const contractAddress = process.env.NFT_COLLECTION_CONTRACT_ADDRESS;
@@ -27,7 +27,7 @@ export async function setLotteryContract() {
         const receipt = await transaction.wait();
         spinner.stop();
 
-        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL}tx/${receipt.transactionHash}`);
+        console.log(`🧾 Transaction URL: ${process.env.BLOCK_EXPLORER_URL_REDDIO}tx/${receipt.transactionHash}`);
         console.log(`✅ Lottery contract successfully established in NFTCollection contract`);
     } catch (error) {
         spinner.stop();
