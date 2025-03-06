90db0536046c1b23e62306c80cfc495294049d07
onedionys <onedionys@gmail.com>
2025-03-06 16:59:51 +0700

    feat: add extra data field to activities and retrieval function

--- contracts/Leaderboard.sol
+++ contracts/Leaderboard.sol
@@ -14,6 +14,7 @@ contract Leaderboard {
 
     mapping(address => Activity[]) public userActivities;
     mapping(address => bool) public isUserRegistered;
+    mapping(string => string[]) public activityExtraData;
 
     event ActivityAdded(
         address indexed user,
@@ -21,7 +22,8 @@ contract Leaderboard {
         string description,
         uint256 amount,
         uint256 timestamp,
-        string txhash
+        string txhash,
+        string[] extraData
     );
 
     function addActivity(
@@ -29,7 +31,8 @@ contract Leaderboard {
         string memory _activity,
         string memory _description,
         uint256 _amount,
-        string memory _txhash
+        string memory _txhash,
+        string[] memory _extraData
     ) public {
         if (!isUserRegistered[_user]) {
             isUserRegistered[_user] = true;
@@ -45,8 +48,13 @@ contract Leaderboard {
         });
 
         userActivities[_user].push(newActivity);
+        activityExtraData[_txhash] = _extraData;
 
-        emit ActivityAdded(_user, _activity, _description, _amount, block.timestamp, _txhash);
+        emit ActivityAdded(_user, _activity, _description, _amount, block.timestamp, _txhash, _extraData);
+    }
+
+    function getExtraData(string memory _txhash) public view returns (string[] memory) {
+        return activityExtraData[_txhash];
     }
 
     function getActivities(address _user) public view returns (Activity[] memory) {
